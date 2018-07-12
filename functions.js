e97cabb3acf63d21c86c6204e0f742feba901bdc
JarlBargluuf <33900088+JarlBargluuf@users.noreply.github.com>
2018-07-12 20:06:54 +0300

    first commit

---
+++ functions.js
@@ -0,0 +1,24 @@
+function sum( x , y ) {
+    return x + y 
+}
+
+console.log( sum ( 2 , 3 ))
+
+function minus( x , y ) {
+    return x - y
+}
+
+console.log( minus ( 5 , 4 ))
+
+function multiplication( x , y ) {
+    return x * y
+}
+
+console.log(multiplication( 8 , 10 ))
+
+function division( x , y ) {
+    return x / y
+}
+
+console.log(division( 8 , 2 ))
+
